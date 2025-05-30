b5755f3b7fc2b0f1fedba60765c2bcaa875ba7f5
Deigratia-D <DrewMistelske@gmail.com>
2025-05-30 12:51:49 -0400

    Port from environment variable.

--- sketch.js
+++ sketch.js
@@ -10,7 +10,8 @@ const fetch = (...args) =>
 	import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const app = express();
-app.listen(3000, () => console.log("Listening at 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Starting server at ${port}.`));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
